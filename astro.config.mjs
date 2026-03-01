f99f143c67b264b461d3fbe08d4f116dc50e1720
Pulsar · 照見 <pulsar@zhaojiian.ai>
2026-03-01 16:39:04 +0800

    fix: add base='/pulsar-web', fix all internal links, update page titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sou350121.github.io/pulsar-web',
+  base: '/pulsar-web',
   output: 'static',
   integrations: [
     sitemap(),
